562218bb995bba8ba45fcb9c75cab9126b4e39a7
alaim.costa <alaim.costa23@gmail.com>
2023-05-09 21:34:18 -0300

    Implementando busca por id específico

--- src/main/java/br/com/altec/projeto/services/GameService.java
+++ src/main/java/br/com/altec/projeto/services/GameService.java
@@ -1,10 +1,12 @@
 package br.com.altec.projeto.services;
 
+import br.com.altec.projeto.dto.GameDTO;
 import br.com.altec.projeto.dto.GameMinDTO;
 import br.com.altec.projeto.entities.Game;
 import br.com.altec.projeto.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +16,20 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    //anotação de boas praticas
+    @Transactional(readOnly = true)//não vou fazer operação de escrita
+    public GameDTO findById(Long id){ //sugestão, fazer tratamento de excessões
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+
+    }
+
+    @Transactional(readOnly = true)//não vou fazer operação de escrita
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
     }
+
+
 
 }
